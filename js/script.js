2a067b09e50e9463f8465aabc03b5de097c41d42
ioanajoanta <ioana_aj@yahoo.com>
2019-06-18 06:29:29 +0100

    add files
    Signed-off-by: ioanajoanta <ioana_aj@yahoo.com>

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * 
+ The product of the first two digits is 24.
+ The fourth digit is one-third of the sec digit.
+The sum of the last two digits are the same as the sum of the first and third digits.
+The sum of all the digits is 26.
+Not all the digits are uniqu
+ */
+function getTheCode() {
+//hide the button 
+
+}
